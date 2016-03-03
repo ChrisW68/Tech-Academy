957c178ed224146673e7135c299b30c6ec1ac1b5
Chris <chris.wiseman74@gmail.com>
2016-03-03 12:47:35 -0800

    Simple C# Application using Entity Framework Code First
    This application is a very simple program for teacher and student
    tracking

---
+++ C# and ASP/Exercise62.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+
+
+namespace EF_Code_First
+{
+    public class Student
+    {
+        public Student()
+        {
+
+        }
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+        public Teacher Teacher { get; set; }
+
+        public Standard Standard { get; set; }
+    }
+
+    public class Teacher
+    {
+        public Teacher()
+        {
+
+        }
+        public int TeacherId { get; set; }
+        public string TeacherName { get; set; }
+    }
+
+    public class Standard
+    {
+        public Standard()
+        {
+
+        }
+        public int StandardId { get; set; }
+        public string StandardName { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                Student stud = new Student() { StudentName = "New Student" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(): base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+    }
+
+}
